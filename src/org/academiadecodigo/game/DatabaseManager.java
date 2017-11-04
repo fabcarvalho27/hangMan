7191c3020ac172ab5306b296bc41a19763b069fa
Fábio Carvalho <fabcarvalho27@gmail.com>
2017-11-04 17:20:34 +0000

    Database basic skeleton

--- src/org/academiadecodigo/game/DatabaseManager.java
+++ src/org/academiadecodigo/game/DatabaseManager.java
@@ -1,4 +1,47 @@
 package org.academiadecodigo.game;
 
+import java.io.File;
+import java.io.InputStreamReader;
+
 public class DatabaseManager {
+
+    InputStreamReader inputStreamReader;
+    File file;
+
+
+    public String pickFilePath(String theme) {
+
+        switch (theme) {
+            case x:
+                return;
+
+            case y:
+
+                return;
+
+            case z:
+
+                return "path";
+
+            default:
+
+                return "default path";
+
+        }
+    }
+
+    public String pickRandomWord(String theme) {
+
+        pickFilePath(theme);
+        selectRandomLine();
+
+
+    }
+
+    public void selectRandomLine() {
+
+    }
+
+    //TODO: make it beautiful!! :)
+    //TODO: create .txt database files by theme
 }
