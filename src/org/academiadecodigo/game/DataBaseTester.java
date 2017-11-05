93ce01470dabfe98a2509423615090a5b050670e
Gustavo Silva <gustavosilva@MacBook-Pro-de-Gustavo.local>
2017-11-05 11:34:42 +0000

    merge with pre-master

---
+++ src/org/academiadecodigo/game/DataBaseTester.java
@@ -0,0 +1,23 @@
+package org.academiadecodigo.game;
+
+public class DataBaseTester {
+
+    public static void main(String[] args) {
+
+        DatabaseManager dataBase = new DatabaseManager();
+
+        for (int i = 0; i < 6 ; i++) {
+
+            dataBase.pickRandomWord("portugues");
+            System.out.println("");
+        }
+
+        for (int i = 0; i < 6 ; i++) {
+
+            dataBase.pickRandomWord("english");
+            System.out.println("");
+        }
+
+
+    }
+}
