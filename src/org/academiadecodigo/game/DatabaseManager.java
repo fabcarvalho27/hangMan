14da23298dcc2102ec94a24e01f28ea0456ca791
Gustavo Silva <gustavosilva@MacBook-Pro-de-Gustavo.local>
2017-11-05 11:00:36 +0000

    database 1.0

--- src/org/academiadecodigo/game/DatabaseManager.java
+++ src/org/academiadecodigo/game/DatabaseManager.java
@@ -1,47 +1,65 @@
 package org.academiadecodigo.game;
 
-import java.io.File;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.LinkedList;
 
 public class DatabaseManager {
 
-    InputStreamReader inputStreamReader;
-    File file;
 
-
-    public String pickFilePath(String theme) {
-
-        switch (theme) {
-            case x:
-                return;
-
-            case y:
-
-                return;
-
-            case z:
-
-                return "path";
-
-            default:
-
-                return "default path";
-
-        }
-    }
+    private File file;
+    private int numberOfWords;
+    private LinkedList<String> wordList;
 
     public String pickRandomWord(String theme) {
 
         pickFilePath(theme);
-        selectRandomLine();
+        System.out.println("File Path: " + file);
+
+        countNumberOfwords();
+        System.out.println("Number of words: " + numberOfWords);
+
+        System.out.println("Random word: " + selectRandomWord());
+        return selectRandomWord();
 
 
     }
 
-    public void selectRandomLine() {
+    public File pickFilePath(String theme) {
+
+        file = new File("resources/" + theme + ".txt");
+        //return String.valueOf(file);
+        return file;
+
 
     }
 
-    //TODO: make it beautiful!! :)
-    //TODO: create .txt database files by theme
+    public void countNumberOfwords() {
+
+        try {
+            numberOfWords = 0;
+            String line;
+            this.wordList = new LinkedList<>();
+
+            BufferedReader bReader = new BufferedReader(new FileReader(file));
+
+            while ((line = bReader.readLine()) != null) {
+                wordList.add(line);
+                numberOfWords++;
+            }
+            bReader.close();
+        } catch (IOException e) {
+            System.out.println("File reading problem");
+        }
+    }
+
+    public String selectRandomWord() {
+        int index = (int) ((Math.random() * numberOfWords));
+        String randomWord = wordList.get(index);
+        //wordList.clear();
+        return randomWord;
+    }
+
+
+//TODO: make it beautiful!! :)
+//TODO: create .txt database files by theme
 }
