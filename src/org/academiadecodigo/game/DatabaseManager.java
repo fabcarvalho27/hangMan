c21ffa2557d33ff57f2f75bc1b1ea5fae9127e47
Gustavo Novais <gnsilva92@gmail.com>
2017-11-06 19:10:43 +0000

    Take away

--- src/org/academiadecodigo/game/DatabaseManager.java
+++ src/org/academiadecodigo/game/DatabaseManager.java
@@ -8,7 +8,6 @@ public class DatabaseManager {
 
     private File file;
     private int numberOfWords;
-    //private LinkedList<String> wordList;
     String[] wordList;
 
     public String pickRandomWord(String theme) {
@@ -19,23 +18,13 @@ public class DatabaseManager {
         countNumberOfWords();
         System.out.println("Number of words: " + numberOfWords);
 
-        System.out.println("Random word: " + selectRandomWord());
-        return selectRandomWord();
+        String word = selectRandomWord();
+        System.out.println("Random word: " + word);
+        return word;
 
     }
 
-    public String pickRandomSentence(String theme) {
 
-        pickFilePath(theme);
-        System.out.println("File Path: " + file);
-
-        countNumberOfWords();
-        //System.out.println("Number of sentences: " + numberOfWords);
-
-        //System.out.println("Random sentence: " + selectRandomWord());
-        return selectRandomWord();
-
-    }
 
 
     public File pickFilePath(String theme) {
@@ -46,29 +35,36 @@ public class DatabaseManager {
 
     }
 
-    /*public void countNumberOfWords() {
+    public void countNumberOfSentences() {
 
         try {
-            numberOfWords = 0;
+
             String line;
-            this.wordList = new LinkedList<>();
+            String longlist = "";
 
             BufferedReader bReader = new BufferedReader(new FileReader(file));
 
             while ((line = bReader.readLine()) != null) {
-                wordList.add(line);
-                numberOfWords++;
+            longlist = longlist.concat(line + " ");
             }
+
             bReader.close();
+            System.out.println("Concatenated text: " + longList);
+
+            sentenceList = longList.split("\\W+");
+
+            numberOfWords = wordList.length;
+            System.out.println("Number of words: " + numberOfWords);
+
+
         } catch (IOException e) {
             System.out.println("File reading problem");
         }
-    }*/
+    }
 
-    public void countNumberOfWords() {
+    /*public void countNumberOfWords() {
 
         try {
-            int numberOfWords;
             String line;
             String longList = "";
 
@@ -81,7 +77,7 @@ public class DatabaseManager {
             bReader.close();
             System.out.println("Concatenated text: " + longList);
 
-            wordList = longList.split(" ");
+            wordList = longList.split("\\W+");
 
             numberOfWords = wordList.length;
             System.out.println("Number of words: " + numberOfWords);
@@ -90,7 +86,7 @@ public class DatabaseManager {
         } catch (IOException e) {
             System.out.println("File reading problem");
         }
-    }
+    }*/
 
     public String selectRandomWord() {
         int index = (int) ((Math.random() * wordList.length));
@@ -113,4 +109,34 @@ public class DatabaseManager {
 
 //TODO: make it beautiful!! :)
 //TODO: create .txt database files by theme
+
+    public String pickSentence(String theme) {
+
+        pickFilePath(theme);
+        System.out.println("File Path: " + file);
+
+        countNumberOfSentences();
+        System.out.println("Number of sentences: " + numberOfWords);
+
+        String sentence = selectRandomWord();
+        System.out.println("Random sentence: " + sentence);
+        return sentence;
+
+    }
+
+    public File pickSentencePath(String theme) {
+
+        file = new File("resources/themes/" + theme + ".txt");
+        return file;
+
+
+    }
+
+
 }
+
+
+
+
+
+
