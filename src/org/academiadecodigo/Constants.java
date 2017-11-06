6aa067ed3575f7ddb52ab98504b5a072582ed520
Fábio Carvalho <fabcarvalho27@gmail.com>
2017-11-06 23:13:35 +0000

    Game, Player, GameTester, GameStatus classes. Game and Player with some basic methods

--- src/org/academiadecodigo/Constants.java
+++ src/org/academiadecodigo/Constants.java
@@ -1,4 +1,14 @@
 package org.academiadecodigo;
 
-public class Constants {
+public abstract class Constants {
+
+    // Server constants
+    public static final int PORT_NUMBER = 9999;
+
+    // Game constants
+    public static final int TOTAL_ROUNDS = 5;
+    public static final int MAX_NUMBER_WRONG_GUESSES = 6;
+
+
+
 }
--- src/org/academiadecodigo/game/Player.java
+++ src/org/academiadecodigo/game/Player.java
@@ -1,4 +1,38 @@
 package org.academiadecodigo.game;
 
+import java.net.Socket;
+
 public class Player {
+
+    private Socket socket;
+
+    private int points;
+    private int numberMissedGuesses;
+    private int numberGuessedLetters;
+
+    public Player(/*Socket socket*/) {
+
+        this.socket = socket;
+
+    }
+
+
+    public String playerGuess() {
+
+        throw new UnsupportedOperationException();
+
+    }
+
+
+    public int getNumberMissedGuesses() {
+        return numberMissedGuesses;
+    }
+
+    public void incrementNumberMissedGuesses() {
+        numberMissedGuesses++;
+    }
+
+    public void incrementNumberGuessedLetters() {
+        numberGuessedLetters++;
+    }
 }
